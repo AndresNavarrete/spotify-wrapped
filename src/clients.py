f2892c12a83b1d1e801880fb90454893f92bd172
Andrés Navarrete <ainavarrete@uc.cl>
2023-04-16 02:49:58 -0400

    remove linter

--- src/clients.py
+++ src/clients.py
@@ -11,6 +11,7 @@ from dotenv import load_dotenv
 # Auth     -  https://developer.spotify.com/documentation/web-api/tutorials/code-flow
 #
 
+
 class Spotify:
     def __init__(self):
         load_dotenv()
