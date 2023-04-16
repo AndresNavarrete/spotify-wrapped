295f7ec7e21aea8103f883e2d695c6ad9c550066
Andrés <42160704+AndresNavarrete@users.noreply.github.com>
2023-04-16 02:50:36 -0400

    add: top artists (#2)
    * add: top artists
    
    * update: code format
    
    * update: lintern
    
    * update: format
    
    * remove linter

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 from src.clients import Spotify
-from src.models import Track
+from src.models import Artist, Track
 
 spotify = Spotify()
 tracks = list()
@@ -8,3 +8,10 @@ items = spotify.get_top_tracks()
 for item in items:
     track = Track(item)
     print(track)
+
+print("\n\n")
+
+items = spotify.get_top_artists()
+for item in items:
+    artist = Artist(item)
+    print(artist)
