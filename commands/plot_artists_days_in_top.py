cc3a0bf9f41fbd60850660439462e8d3d13289d4
AndresNavarrete <andres.navarrete.dev@gmail.com>
2023-07-30 21:07:16 -0400

    uodate: commands

---
+++ commands/plot_artists_days_in_top.py
@@ -0,0 +1,9 @@
+from src.queries_manager import Queries_Manager
+from src.ranking_plotter import RankingImageGenerator
+
+if __name__ == "__main__":
+    query = Queries_Manager().get_sql_script("plot_artist_days_in_top")
+    RankingImageGenerator(
+        query, title="Top artists - days in top 10"
+    ).save_ranking_image("ranking_artists.png")
+
