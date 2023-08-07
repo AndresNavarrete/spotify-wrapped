d246c3e5a2c3466b53f951354360985e42fc087c
AndresNavarrete <andres.navarrete.dev@gmail.com>
2023-08-07 18:18:45 -0400

    add: basic auth

--- django_spotify/django_spotify/settings.py
+++ django_spotify/django_spotify/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django_spotify.basic_auth_middleware.BasicAuthMiddleware"
 ]
 
 ROOT_URLCONF = "django_spotify.urls"
