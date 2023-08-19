353fcbea73d1d878939055f24d50d81e96577dc3
Andrés <42160704+AndresNavarrete@users.noreply.github.com>
2023-08-19 18:54:08 -0400

    Fix/cronjob (#18)
    * update: pipenv location
    
    * update: rankings

--- bash/sql_transformations.sh
+++ bash/sql_transformations.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo sql_transformations
-cd $ROOT_PATH && pipenv run python3 commands/run_all_queries.py 
+cd $ROOT_PATH && /usr/local/bin/pipenv run python3 commands/run_all_queries.py 
